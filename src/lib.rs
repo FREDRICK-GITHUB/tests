c1917b6be4755b70db9780532f2e2329df43e839
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-12-12 09:00:52 +0300

    example to show running of subset of tests with different -- --arg arguments

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width < other.width && self.height > other.height
-    }
+pub fn add_two (a: i32) -> i32 {
+    a + 2
 }
 
 #[cfg(test)]
@@ -19,43 +7,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn sum_test() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn add_two_and_two () {
+        assert_eq!(4, add_two(2))
     }
 
     #[test]
-    fn another () {
-        panic!("Make this test fail");
+    #[ignore]
+    fn three_and_two () {
+        assert_eq!(5, add_two(3))
     }
 
     #[test]
-    fn larger_can_hold_smaller() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
-
-        assert!(larger.can_hold(&smaller));
-    }
-
-    #[test]
-    fn smaller_cannot_hold_larger() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
-
-        assert!(!smaller.can_hold(&larger))
+    fn one_hundred () {
+        assert_eq!(102, add_two(100))
     }
 }
